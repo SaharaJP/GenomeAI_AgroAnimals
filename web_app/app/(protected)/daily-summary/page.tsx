73a2e17d1ece75fe614cf13aa93a3a2d2fce1113
SaharaJP <icreem714@gmail.com>
2026-04-26 01:17:23 +0300

    fix(daily-summary): remove DailyOperationsDashboard from Обзор page

--- web_app/app/(protected)/daily-summary/page.tsx
+++ web_app/app/(protected)/daily-summary/page.tsx
@@ -1,11 +1,5 @@
 import { MorningBriefCard } from '@/components/overview/morning-brief-card';
-import { DailyOperationsDashboard } from '@/components/operations/daily-operations-dashboard';
 
 export default function DailySummaryPage() {
-  return (
-    <>
-      <MorningBriefCard />
-      <DailyOperationsDashboard />
-    </>
-  );
+  return <MorningBriefCard />;
 }
